cee991c7b0566b6892e52a5147134df3ef2c4e00
ranahusain <capthussain2point0@gmail.com>
2025-06-30 16:58:24 +0500

    middleware create

--- index.js
+++ index.js
@@ -2,7 +2,27 @@ const express = require("express");
 const app = express();
 const Port = 3000;
 
+//inBuilt Middleware
 app.use(express.json());
+
+//Custom Middleware
+//logging,athentication,validation
+
+const logginMiddleware = function (req, res, next) {
+  console.log("logging Middlware");
+  next();
+};
+
+const authMiddleware = function (req, res, next) {
+  console.log("Authentication Middleware");
+  next();
+};
+
+const validationMiddleware = function (req, res, next) {
+  console.log("validation Middleware");
+  next();
+};
+
 app.get("/", (req, res) => {
   console.log(req.body);
   res.send("Hello World");
